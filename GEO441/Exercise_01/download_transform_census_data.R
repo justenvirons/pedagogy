8c7e3617c0a686d7015d7169ffa4fac9ee648579
justenvirons <justenvirons@gmail.com>
2022-03-29 17:44:08 -0500

    update files

--- GEO441/Exercise_01/download_transform_census_data.R
+++ GEO441/Exercise_01/download_transform_census_data.R
@@ -24,14 +24,15 @@ library(tidyverse)
 library(dplyr) 
 library(sf) 
 
+# Part One: Select profile community, download, transform spatial  --------
 # Download  census geographies
 # cb = cartographic boundary
+IL_State_geom <- states(resolution='500k', cb=TRUE, class="sf")
 IL_Counties_geom <- counties("IL", cb=TRUE, class="sf")
 IL_Tracts_geom <- tracts("IL", cb=TRUE, class="sf")
-IL_Places_geom <- places("IL", cb=TRUE, class="sf")
+IL_Places_geom <- places("US", cb=TRUE, class="sf")
 
-plot(IL_Places_geom["GEOID"])
-
+# Project to appropriate UTM zone
 # Project to UTM North Zone 16
 IL_Counties_geom <- st_transform(IL_Counties_geom, crs = 26916)
 IL_Tracts_geom <- st_transform(IL_Tracts_geom, crs = 26916)
@@ -47,22 +48,33 @@ CookCounty_geom <- IL_Counties_geom %>%
 # https://api.census.gov/data/key_signup.html
 
 apis <- listCensusApis()
-View(apis)
 
-# Download complete list of ACS 5-year tables
+# Download latest list of ACS 5-year tables
 # source api: https://api.census.gov/data/2019/acs/acs5
 
-ayear <- "2019"
+ayear <- "2020"
+
+# download detailed table list
 acs_groups_tables <- listCensusMetadata(
   name = "acs/acs5",
   vintage = ayear,
   type = "groups")
 acs_groups_tables$year<-ayear # add year variable
-assign(paste("acs_5yr_tables_",ayear,sep=""),acs_groups_tables) # change name of dataframe
+assign(paste("acs_5yr_tables_detailed_",ayear,sep=""),acs_groups_tables) # change name of dataframe
 rm(acs_groups_tables)
 
-# Variables for ACS data table
-ayear <- "2019"
+# download subject tables list
+acs_groups_tables <- listCensusMetadata(
+  name = "acs/acs5/subject",
+  vintage = ayear,
+  type = "groups")
+acs_groups_tables$year<-ayear # add year variable
+assign(paste("acs_5yr_tables_subject_",ayear,sep=""),acs_groups_tables) # change name of dataframe
+rm(acs_groups_tables)
+
+# download variables for ACS data table
+
+# use for detailed tables 
 agroup <- "B01001"
 acs_groups_vars <- listCensusMetadata(
   name = "acs/acs5",
@@ -78,35 +90,27 @@ acs_groups_vars <- acs_groups_vars %>%
 assign(paste("table_",agroup,"_",ayear, sep=""),acs_groups_vars)
 rm(acs_groups_vars)
 
+# use for subject tables
+agroup <- "B01001"
+acs_groups_vars <- listCensusMetadata(
+  name = "acs/acs5/subject",
+  vintage = ayear,
+  group = agroup,
+  type = "variables")
+acs_groups_vars$year<-ayear
+acs_groups_vars <- acs_groups_vars %>% 
+  filter(!str_detect(name,"EA"),!str_detect(name,"M")) %>% 
+  mutate(label = str_replace_all(label, "Estimate!!",""),
+         label = str_replace(label, "!!"," "),
+         label = str_replace_all(label, "!!"," "))
+assign(paste("table_",agroup,"_",ayear, sep=""),acs_groups_vars)
+rm(acs_groups_vars)
+
 # Use write_clip to copy to clipboard (and paste to Excel or other program) or
 # use write_csv to write data table to computer
-write_clip(groupvars_B28001_2019)
-write_csv(groupvars_B28001_2019,"C:/Users/scott/Desktop/delete/groupvars_B28001_2019.csv")
 
-# The following code allows you to download datasets from multiple tables
-grouplist <- c("B01001")
+# example
+write_clip(groupvars_B28001_2020)
+write_csv(groupvars_B28001_2020,"C:/Users/scott/Desktop/delete/groupvars_B28001_2019.csv")
 
-# Download places
-yearlist <- c(2019)
-for (agroup in grouplist) {
-  for (ayear in yearlist) {
-    agroupname = paste("group(",agroup,")",sep="")
-    acs_group <- getCensus(name = "acs/acs5",
-                           vintage = ayear,
-                           vars = c("NAME", agroupname),
-                           region = "zip code tabulation area:*", # tracts
-                           regionin="state:17", # places, counties, not msas
-                           key="8f6a0a83c8a2466e3e018a966846c86412d0bb6e")
-    attach(acs_group)
-    acs_group <- acs_group %>% select(-contains("EA"))
-    acs_group <- acs_group %>% select(-contains("MA"))
-    acs_group <- acs_group %>% select(-contains("GEO_ID"))
-    acs_group <- acs_group %>% select(-contains("M_1"))
-    acs_group <- acs_group %>% select(-contains("M"))
-    acs_group$year<-ayear 
-    acs_group$GEOID_place<-paste0(state,place)
-    assign(paste(agroup,name,ayear,sep="_"),acs_group)
-    rm(acs_group)
-    detach(acs_group)
-  }
-}
+
